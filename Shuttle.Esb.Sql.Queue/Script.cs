64965c11a3436e1880861b0c710c7eb590ab69f5
Eben <me@ebenroux.co.za>
2017-03-12 20:40:28 +0200

    - removed script constructor / properties

--- Shuttle.Esb.Sql.Queue/Script.cs
+++ Shuttle.Esb.Sql.Queue/Script.cs
@@ -1,6 +1,3 @@
-using System.Configuration;
-using Shuttle.Core.Infrastructure;
-
 namespace Shuttle.Esb.Sql.Queue
 {
 	public class Script
@@ -21,32 +18,5 @@ namespace Shuttle.Esb.Sql.Queue
 		public static readonly string DeferredMessageDequeue = "DeferredMessageDequeue";
 		public static readonly string DeferredMessagePurge = "DeferredMessagePurge";
 		public static readonly string DeferredMessageCount = "DeferredMessageCount";
-
-		private Script(string name)
-		{
-			Name = name;
-
-			var key = string.Format("{0}FileName", name);
-
-			var value = ConfigurationManager.AppSettings[key];
-
-			if (!string.IsNullOrEmpty(value))
-			{
-				FileName = value;
-
-				return;
-			}
-
-			FileName = string.Format("{0}.sql", name);
-
-			Log.For(this)
-				.Information(
-					string.Format(
-						"The application configuration AppSettings section does not contain a key '{0}'.  Using default value of '{1}'.",
-						key, FileName));
-		}
-
-		public string Name { get; private set; }
-		public string FileName { get; private set; }
 	}
 }

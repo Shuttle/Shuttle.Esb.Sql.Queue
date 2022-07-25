20fcc174b031d0a945a8befb259f71f583b203e9
Eben <me@ebenroux.co.za>
2022-07-25 09:54:51 +0200

    Update SqlOutboxFixture.cs

--- Shuttle.Esb.Sql.Queue.Tests/SqlOutboxFixture.cs
+++ Shuttle.Esb.Sql.Queue.Tests/SqlOutboxFixture.cs
@@ -10,7 +10,7 @@ namespace Shuttle.Esb.Sql.Queue.Tests
 		[TestCase(true)]
 		public void Should_be_able_handle_errors(bool isTransactionalEndpoint)
 		{
-			TestOutboxSending(SqlFixture.GetServiceCollection(), "sql://shuttle/{0}", isTransactionalEndpoint);
+			TestOutboxSending(SqlFixture.GetServiceCollection(), "sql://shuttle/{0}", 1, isTransactionalEndpoint);
 		}
 	}
 }
